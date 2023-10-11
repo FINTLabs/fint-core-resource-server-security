f6e85e57aa56478725346d9538c72de4aa9f99ed
Hknots <Hknots99@gmail.com>
2023-10-11 12:23:59 +0200

    add configuration annotation to securityConfig

--- src/main/java/no/fintlabs/resourceServer/security/SecurityConfiguration.java
+++ src/main/java/no/fintlabs/resourceServer/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package no.fintlabs.resourceServer.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 }
