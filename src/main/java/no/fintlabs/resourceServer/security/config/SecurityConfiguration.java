8c3804b182e4daa3113d783aa349cb58dce214ba
Hknots <Hknots99@gmail.com>
2023-10-11 12:28:35 +0200

    override securityWebFilterChain

--- src/main/java/no/fintlabs/resourceServer/security/config/SecurityConfiguration.java
+++ src/main/java/no/fintlabs/resourceServer/security/config/SecurityConfiguration.java
@@ -1,12 +1,22 @@
 package no.fintlabs.resourceServer.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    private final FintSecurity fintSecurity;
 
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.build();
+    }
 
 }
