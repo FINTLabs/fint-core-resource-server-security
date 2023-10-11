ed8cc67b927710ae36340a3c4b6e657984545bba
Hknots <Hknots99@gmail.com>
2023-10-11 12:25:23 +0200

    add config package

--- src/main/java/no/fintlabs/resourceServer/security/config/FintSecurity.java
+++ src/main/java/no/fintlabs/resourceServer/security/config/FintSecurity.java
@@ -1,4 +1,4 @@
-package no.fintlabs.resourceServer.security;
+package no.fintlabs.resourceServer.security.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/no/fintlabs/resourceServer/security/config/SecurityConfiguration.java
+++ src/main/java/no/fintlabs/resourceServer/security/config/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package no.fintlabs.resourceServer.security;
+package no.fintlabs.resourceServer.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
