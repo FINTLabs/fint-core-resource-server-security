5cc12331623e3bad942562ffeffc47e5d3f7728c
Hknots <Hknots99@gmail.com>
2023-10-11 15:43:58 +0200

    add corePrincipal

---
+++ src/main/java/no/fintlabs/resourceServer/security/CorePrincipal.java
@@ -0,0 +1,28 @@
+package no.fintlabs.resourceServer.security;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@Getter
+public class CorePrincipal extends JwtAuthenticationToken {
+
+    private final String orgId;
+    private final String scope;
+
+    @Getter(AccessLevel.NONE)
+    private final HashSet<String> roles;
+
+    public CorePrincipal(Jwt jwt, Collection<? extends GrantedAuthority> authorities, String orgId, String scope, HashSet<String> roles) {
+        super(jwt, authorities);
+        this.orgId = orgId;
+        this.scope = scope;
+        this.roles = roles;
+    }
+
+}
