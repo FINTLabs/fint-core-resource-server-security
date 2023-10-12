5d60cd96d0e75fac91a9dfbc295daac7660e6292
Hknots <Hknots99@gmail.com>
2023-10-12 09:38:09 +0200

    add access method to validate request based on config

--- src/main/java/no/fintlabs/resourceServer/security/config/SecurityConfiguration.java
+++ src/main/java/no/fintlabs/resourceServer/security/config/SecurityConfiguration.java
@@ -1,20 +1,27 @@
 package no.fintlabs.resourceServer.security.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.resourceServer.security.CorePrincipal;
 import no.fintlabs.resourceServer.security.CorePrincipalConverter;
-import no.vigoiks.resourceserver.security.FintJwtDefaultConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import reactor.core.publisher.Mono;
 
+@Slf4j
 @Configuration
 @EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
     private final FintSecurity fintSecurity;
+    private final ConsumerConfig consumerConfig;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -31,9 +38,35 @@ public class SecurityConfiguration {
                 .authorizeExchange(authorizeExchangeSpec ->
                         authorizeExchangeSpec
                                 .anyExchange()
-                                .authenticated());
+                                .access(this::checkCorePrincipalForAccess));
 
         return http.build();
+    }
+
+    private Mono<AuthorizationDecision> checkCorePrincipalForAccess(Mono<Authentication> authentication,
+                                                                    AuthorizationContext context) {
+        return authentication.map(auth -> {
+            if (auth instanceof CorePrincipal corePrincipal) {
+                boolean componentRequired = fintSecurity.isComponentRequired();
+                boolean orgIdRequired = fintSecurity.isOrgIdRequired();
+
+                if (!componentRequired && !orgIdRequired) {
+                    return new AuthorizationDecision(true);
+                }
+
+                boolean isComponentValid = !componentRequired || corePrincipal.hasRole(getComponentRole());
+                boolean isOrgIdValid = !orgIdRequired || corePrincipal.orgIdsMatch(consumerConfig.getOrgId());
+
+                return new AuthorizationDecision(isComponentValid && isOrgIdValid);
+            }
+
+            log.warn("(SecurityConfiguration): Jwt is not a CorePrincipal! ");
+            return new AuthorizationDecision(false);
+        });
+    }
+
+    private String getComponentRole() {
+        return String.format("FINT_Client_%s", consumerConfig.getComponent());
     }
 
     private SecurityWebFilterChain permitAll(ServerHttpSecurity http) {
