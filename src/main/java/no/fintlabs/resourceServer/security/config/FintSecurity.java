2549c126943a5c9eeaf41ed23dda831e002fda14
Hknots <Hknots99@gmail.com>
2023-10-12 09:33:00 +0200

    change to use only Configuration & add default values

--- src/main/java/no/fintlabs/resourceServer/security/config/FintSecurity.java
+++ src/main/java/no/fintlabs/resourceServer/security/config/FintSecurity.java
@@ -2,20 +2,18 @@ package no.fintlabs.resourceServer.security.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties("fint.security")
 public class FintSecurity {
 
+    @Value("${fint.security.enabled:true}")
     private boolean enabled;
 
-    @Value("${fint.security.orgid}")
+    @Value("${fint.security.orgid:true}")
     private boolean orgIdRequired;
 
-    @Value("${fint.security.component}")
+    @Value("${fint.security.component:true}")
     private boolean componentRequired;
-
 }
