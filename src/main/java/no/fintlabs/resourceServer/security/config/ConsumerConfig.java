d5b556bfe881041b145e620289a015363b721e4f
Hknots <Hknots99@gmail.com>
2023-10-12 09:34:41 +0200

    add consumerconfig

---
+++ src/main/java/no/fintlabs/resourceServer/security/config/ConsumerConfig.java
@@ -0,0 +1,20 @@
+package no.fintlabs.resourceServer.security.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+public class ConsumerConfig {
+
+    @Value("${fint.consumer.domain}")
+    private String domain;
+
+    @Value("${fint.consumer.package}")
+    private String packageName;
+
+    @Value("${fint.consumer.orgId}")
+    private String orgId;
+
+}
