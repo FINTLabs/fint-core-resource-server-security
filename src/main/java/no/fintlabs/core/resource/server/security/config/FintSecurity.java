1d45f92f56ea25f7ea9ab8ab3b1d284bfa9ea5e0
Hknots <Hknots99@gmail.com>
2023-10-17 21:14:36 +0200

    add default value to roletype if not valid

--- src/main/java/no/fintlabs/core/resource/server/security/config/FintSecurity.java
+++ src/main/java/no/fintlabs/core/resource/server/security/config/FintSecurity.java
@@ -1,9 +1,11 @@
 package no.fintlabs.core.resource.server.security.config;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Data
 @Configuration
 public class FintSecurity {
@@ -47,7 +49,8 @@ public class FintSecurity {
                     return roleType;
                 }
             }
-            throw new IllegalArgumentException("Invalid role type: " + value);
+            log.error("Invalid role type: {} Defaulting to Client.", value);
+            return CLIENT;
         }
 
         @Override
