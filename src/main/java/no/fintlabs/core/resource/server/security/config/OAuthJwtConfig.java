4c7d9ada2ccec4cf3690c42eb5da663bce548eef
Hknots <Hknots99@gmail.com>
2023-10-31 15:19:13 +0100

    add comment about the purpose of the service

--- src/main/java/no/fintlabs/core/resource/server/security/config/OAuthJwtConfig.java
+++ src/main/java/no/fintlabs/core/resource/server/security/config/OAuthJwtConfig.java
@@ -3,6 +3,11 @@ package no.fintlabs.core.resource.server.security.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+
+/**
+ * Retrieves the JWT Issuer URI for the application.
+ * Eliminates the necessity for apps to configure this separately.
+ */
 @Configuration
 @PropertySource(value = "classpath:oauth-jwt-settings/jwt-config.properties")
 public class OAuthJwtConfig {
