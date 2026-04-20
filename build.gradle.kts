bacfe9307c3ba59f8a5d5a47e69975b554c8a2f4
Hknots <hknots99@gmail.com>
2026-04-20 21:26:37 +0200

    chore(build): use standard test layout and drop custom source sets
    - Move tests from src/test/unit/kotlin to src/test/kotlin (Gradle default)
    - Delete unused src/test/unit and src/test/integration directories
    - Replace configureTestSets / configureTestConfigs / configureTestTasks
      helpers with the default `test` task
    - Drop unused top-of-file imports (Gradle Kotlin DSL provides them)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-import org.gradle.api.Project
-import org.gradle.api.tasks.bundling.Jar
-import org.gradle.api.tasks.testing.Test
-import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.gradle.kotlin.dsl.*
-
 plugins {
 	kotlin("jvm") version "2.3.0"
 	kotlin("plugin.spring") version "2.3.0"
@@ -39,8 +32,8 @@ kotlin {
 	compilerOptions { freeCompilerArgs.addAll("-Xjsr305=strict") }
 }
 
-tasks.withType<Test> { useJUnitPlatform() }
-tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootJar> { enabled = false }
+tasks.test { useJUnitPlatform() }
+tasks.bootJar { enabled = false }
 
 val sourcesJar by tasks.registering(Jar::class) {
 	archiveClassifier.set("sources")
@@ -54,41 +47,3 @@ publishing {
 		artifact(sourcesJar.get())
 	}
 }
-
-fun Project.configureTestSets() = sourceSets {
-	val unit by creating {
-		kotlin.srcDir("src/test/unit/kotlin")
-		resources.srcDir("src/test/unit/resources")
-		compileClasspath += main.get().output + configurations["testRuntimeClasspath"]
-		runtimeClasspath += output + compileClasspath
-	}
-	val integration by creating {
-		kotlin.srcDir("src/test/integration/kotlin")
-		resources.srcDir("src/test/integration/resources")
-		compileClasspath += main.get().output + configurations["testRuntimeClasspath"]
-		runtimeClasspath += output + compileClasspath
-	}
-}
-
-fun Project.configureTestConfigs() = listOf("unit", "integration")
-	.forEach {
-		configurations["${it}Implementation"].extendsFrom(configurations["testImplementation"])
-		configurations["${it}RuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
-	}
-
-fun Project.configureTestTasks() {
-	tasks.register<Test>("unitTest") {
-		testClassesDirs = sourceSets["unit"].output.classesDirs
-		classpath = sourceSets["unit"].runtimeClasspath
-	}
-	tasks.register<Test>("integrationTest") {
-		shouldRunAfter("unitTest")
-		testClassesDirs = sourceSets["integration"].output.classesDirs
-		classpath = sourceSets["integration"].runtimeClasspath
-	}
-	tasks.named("check") { dependsOn("unitTest", "integrationTest") }
-}
-
-configureTestSets()
-configureTestConfigs()
-configureTestTasks()
